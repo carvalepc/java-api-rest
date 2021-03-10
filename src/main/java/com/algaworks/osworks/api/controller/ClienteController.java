0e6bb5af22c53b8aad821667763b51e1ca3c625e
Luiz Carvalho <luiz.carvalho@alcoa.com>
2021-03-10 18:21:52 -0300

    Aula01 - Osworks 38 min

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -3,6 +3,9 @@ package com.algaworks.osworks.api.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.*;
 
 import com.algaworks.osworks.domain.model.Cliente;
@@ -11,23 +14,13 @@ import com.algaworks.osworks.domain.model.Cliente;
 
 public class ClienteController {
 
-	@GetMapping("/clientes")
+	@PersistenceContext
+	private EntityManager manager;
 	
+	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Luiz Eduardo Carvalho");
-		cliente1.setEmail("luiz@gmail.com");
-		cliente1.setTelefone("35991330239");
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 			
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Deborah Carvalho");
-		cliente2.setEmail("Deborah@gmail.com");
-		cliente2.setTelefone("35988629685");
-		
-		return Arrays.asList(cliente1, cliente2);
-		
 	}
 }
