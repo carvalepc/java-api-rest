de02c668a115aff069a5006fa86d942845ba7bf1
Luiz Carvalho <luiz.carvalho@alcoa.com>
2021-03-12 20:40:47 -0300

    Aula02 - Osworks -  Crud 1:05

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.algaworks.osworks.domain.model.Cliente;
@@ -12,24 +13,59 @@ import com.algaworks.osworks.domain.repository.ClienteRepository;
 
 
 @RestController
+@RequestMapping("/clientes")
 
 public class ClienteController {
 
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
-	@GetMapping("/clientes")
+	@GetMapping
 	public List<Cliente> listar() {
 		return clienteRepository.findAll();			
 	}
 	
-	@GetMapping("/clientes/{clienteId}")
+	@GetMapping("/{clienteId}")
 	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
 		Optional<Cliente> cliente =  clienteRepository.findById(clienteId);
 		if(cliente.isPresent()) {
 			return ResponseEntity.ok(cliente.get());
 		}
 		return ResponseEntity.notFound().build();
+		
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	
+	public Cliente adicionar(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizar (@PathVariable Long clienteId, 
+			@RequestBody Cliente cliente){
+
+		if (!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		cliente.setId(clienteId);
+		cliente = clienteRepository.save(cliente);
+		
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/{clienteId}")
+	public ResponseEntity<Void> remover(@PathVariable Long clienteId){
+		
+
+		if (!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		clienteRepository.deleteById(clienteId);
+		
+		return ResponseEntity.noContent().build();
 		
 	}
 }
