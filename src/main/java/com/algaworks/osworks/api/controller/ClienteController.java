234a6b74e627a396b87c1bff8df2233b29c277f1
Luiz Carvalho <luiz.carvalho@alcoa.com>
2021-03-12 19:59:57 -0300

    Aula02 - Osworks 38 min

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -1,26 +1,36 @@
 package com.algaworks.osworks.api.controller;
 
-import java.util.Arrays;
+
 import java.util.List;
+import java.util.Optional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import com.algaworks.osworks.domain.model.Cliente;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
+
 
 @RestController
 
 public class ClienteController {
 
-	@PersistenceContext
-	private EntityManager manager;
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		return manager.createQuery("from Cliente", Cliente.class)
-				.getResultList();
-			
+		return clienteRepository.findAll();			
+	}
+	
+	@GetMapping("/clientes/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		Optional<Cliente> cliente =  clienteRepository.findById(clienteId);
+		if(cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		return ResponseEntity.notFound().build();
+		
 	}
 }
+	
