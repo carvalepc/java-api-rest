884f8b4ee64369e22eef475784c80e16e9def117
Luiz Carvalho <luiz.carvalho@alcoa.com>
2021-03-13 13:19:25 -0300

    Aula02 - Osworks -  Final

---
+++ src/main/java/com/algaworks/osworks/api/Exception/NegocioException.java
@@ -0,0 +1,13 @@
+package com.algaworks.osworks.api.Exception;
+
+public class NegocioException extends RuntimeException {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    
+    public NegocioException(String message){
+        super(message);
+    }
+}
--- src/main/java/com/algaworks/osworks/domain/model/Cliente.java
+++ src/main/java/com/algaworks/osworks/domain/model/Cliente.java
@@ -13,45 +13,55 @@ import javax.validation.constraints.Size;
 public class Cliente {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(name = "nome")	
 	@NotBlank
 	@Size(max = 60)
 	private String nome;
 	
 	@NotBlank
 	@Email
+	@Size(max = 255)
 	private String email;
 	
 	@NotBlank
 	@Size(max = 20)
+	@Column(name = "fone")
 	private String telefone;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getTelefone() {
 		return telefone;
 	}
+
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -59,6 +69,7 @@ public class Cliente {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -75,7 +86,5 @@ public class Cliente {
 			return false;
 		return true;
 	}
-	
-	
-	
+
 }
